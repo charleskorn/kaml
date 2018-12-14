063b2d938c213fedbb3c11b0471d6988704143f2
Charles Korn <me@charleskorn.com>
2018-12-13 20:03:24 -0600

    Add license to source files.

--- src/test/kotlin/com/charleskorn/kaml/YamlMapTest.kt
+++ src/test/kotlin/com/charleskorn/kaml/YamlMapTest.kt
@@ -1,11 +1,27 @@
+/*
+
+   Copyright 2018 Charles Korn.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+
 package com.charleskorn.kaml
 
 import ch.tutteli.atrium.api.cc.en_GB.message
 import ch.tutteli.atrium.api.cc.en_GB.toBe
 import ch.tutteli.atrium.api.cc.en_GB.toThrow
-import ch.tutteli.atrium.domain.builders.AssertImpl
 import ch.tutteli.atrium.domain.creating.throwable.thrown.ThrowableThrown
-import ch.tutteli.atrium.reporting.translating.Untranslatable
 import ch.tutteli.atrium.verbs.assert
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
@@ -191,4 +207,3 @@ object YamlMapTest : Spek({
 
 // FIXME: This is a hack, pending further discussion at https://kotlinlang.slack.com/archives/C887ZKGCQ/p1544305394000500
 fun ThrowableThrown.Builder.toNotThrow() = act()
-
