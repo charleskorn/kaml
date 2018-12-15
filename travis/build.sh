f8f26333b8540f708c4db4c98543e099a1f08b62
Charles Korn <me@charleskorn.com>
2018-12-15 11:41:39 -0600

    Add a basic Travis setup.

---
+++ travis/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "Building..."
+./batect build
+echo
+
+echo "Running unit tests and static analysis..."
+./batect check
+echo
+
+echo "Generating code coverage report..."
+./batect generateCodeCoverageReport
+echo
