fb73d78acc691b2dd7e3180f131384237f300b7d
Charles Korn <me@charleskorn.com>
2018-12-16 11:04:13 -0600

    Apply Spotless to buildSrc too.

--- buildSrc/src/main/kotlin/com/charleskorn/kaml/build/SpotlessConfiguration.kt
+++ buildSrc/src/main/kotlin/com/charleskorn/kaml/build/SpotlessConfiguration.kt
@@ -1,3 +1,21 @@
+/*
+
+   Copyright 2018 Charles Korn.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+
 package com.charleskorn.kaml.build
 
 import com.diffplug.gradle.spotless.SpotlessExtension
@@ -61,6 +79,7 @@ fun Project.configureSpotless() {
         }
 
         kotlin {
+            target("src/**/*.kt", "buildSrc/**/*.kt")
             ktlint("0.29.0")
 
             @Suppress("INACCESSIBLE_TYPE")
