1e681a7d54adfab3ea4e82c195667eb31d2882e4
Charles Korn <me@charleskorn.com>
2019-01-31 17:51:10 +0900

    Update to release version of JaCoCo 0.8.3.

--- buildSrc/src/main/kotlin/com/charleskorn/kaml/build/JacocoConfiguration.kt
+++ buildSrc/src/main/kotlin/com/charleskorn/kaml/build/JacocoConfiguration.kt
@@ -21,9 +21,7 @@ package com.charleskorn.kaml.build
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.maven
 import org.gradle.kotlin.dsl.named
-import org.gradle.kotlin.dsl.repositories
 import org.gradle.testing.jacoco.plugins.JacocoPlugin
 import org.gradle.testing.jacoco.plugins.JacocoPluginExtension
 import org.gradle.testing.jacoco.tasks.JacocoReport
@@ -31,12 +29,8 @@ import org.gradle.testing.jacoco.tasks.JacocoReport
 fun Project.configureJacoco() {
     apply<JacocoPlugin>()
 
-    repositories {
-        maven("https://oss.sonatype.org/content/repositories/snapshots")
-    }
-
     configure<JacocoPluginExtension> {
-        toolVersion = "0.8.3-SNAPSHOT"
+        toolVersion = "0.8.3"
     }
 
     tasks.named<JacocoReport>("jacocoTestReport") {
