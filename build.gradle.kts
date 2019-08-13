d4ff44984eb7fed2d903a42a5a2785c6615fa41c
Charles Korn <me@charleskorn.com>
2019-08-13 21:51:05 +1000

    Update Versions plugin to latest version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ buildscript {
 plugins {
     kotlin("jvm") version "1.3.41"
 
-    apply { id("com.github.ben-manes.versions") version "0.21.0" }
+    apply { id("com.github.ben-manes.versions") version "0.22.0" }
 }
 
 apply(plugin = "kotlinx-serialization")
