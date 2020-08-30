d6f1dcc929dfe8a6fa60ba15b03cf8682c118b5b
Charles Korn <me@charleskorn.com>
2020-08-30 14:01:47 +1000

    Update expected copyright year.

--- src/test/kotlin/com/charleskorn/kaml/YamlNodeReaderTest.kt
+++ src/test/kotlin/com/charleskorn/kaml/YamlNodeReaderTest.kt
@@ -1,6 +1,6 @@
 /*
 
-   Copyright 2018-2019 Charles Korn.
+   Copyright 2018-2020 Charles Korn.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
