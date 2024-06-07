12c2c32ab88ecf865391763f0d5c86974a8ca8a5
Piotr Krzeminski <git@krzeminski.it>
2024-06-07 06:56:58 +0200

    Update Kotlin to 2.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ rootProject.name = "kaml"
 
 pluginManagement {
     plugins {
-        kotlin("multiplatform") version "1.9.24"
-        kotlin("plugin.serialization") version "1.9.24"
+        kotlin("multiplatform") version "2.0.0"
+        kotlin("plugin.serialization") version "2.0.0"
     }
 }
