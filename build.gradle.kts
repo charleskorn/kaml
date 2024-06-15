cce9daed0d05c788f380711a1f43bf46c1253361
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2024-06-15 16:03:05 +0200

    Disable wasm tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,8 +63,20 @@ kotlin {
 
     wasmJs {
         binaries.library()
-        browser()
-        nodejs()
+        browser {
+            testTask {
+                // TODO: enable once the tests work with Kotlin/Wasm.
+                // See https://youtrack.jetbrains.com/issue/KT-68950/
+                enabled = false
+            }
+        }
+        nodejs {
+            testTask {
+                // TODO: enable once the tests work with Kotlin/Wasm.
+                // See https://youtrack.jetbrains.com/issue/KT-68950/
+                enabled = false
+            }
+        }
     }
 
     sourceSets {
