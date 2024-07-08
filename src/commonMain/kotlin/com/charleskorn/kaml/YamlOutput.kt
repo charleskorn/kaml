76ac8dd0ef23075eff0644dc7274b0db0f90d634
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2024-07-08 18:59:20 +0530

    feat: encode lowercase enums

--- src/commonMain/kotlin/com/charleskorn/kaml/YamlOutput.kt
+++ src/commonMain/kotlin/com/charleskorn/kaml/YamlOutput.kt
@@ -93,7 +93,15 @@ internal class YamlOutput(
         }
     }
 
-    override fun encodeEnum(enumDescriptor: SerialDescriptor, index: Int) = emitQuotedScalar(enumDescriptor.getElementName(index), configuration.singleLineStringStyle.scalarStyle)
+    override fun encodeEnum(enumDescriptor: SerialDescriptor, index: Int) {
+        val element = if (configuration.decodeEnumCaseInsensitive) { // use lower case enums
+            enumDescriptor.getElementName(index).lowercase()
+        } else {
+            enumDescriptor.getElementName(index)
+        }
+
+        emitQuotedScalar(element, configuration.singleLineStringStyle.scalarStyle)
+    }
 
     private fun emitPlainScalar(value: String) = emitScalar(value, ScalarStyle.PLAIN)
     private fun emitQuotedScalar(value: String, scalarStyle: ScalarStyle) = emitScalar(value, scalarStyle)
