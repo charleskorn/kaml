ac21a99750b9eff4900a6c1b066895f8dfb9b5a2
mani1232 <support@worldmandia.cc>
2024-07-17 16:36:32 +0200

    Use core

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,7 +75,7 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
-                api("org.jetbrains.kotlinx:kotlinx-serialization-json-io:1.7.1")
+                api("org.jetbrains.kotlinx:kotlinx-serialization-core:1.7.1")
                 implementation("it.krzeminski:snakeyaml-engine-kmp:3.0.1")
                 implementation("org.jetbrains.kotlinx:kotlinx-io-core:0.5.1")
             }
