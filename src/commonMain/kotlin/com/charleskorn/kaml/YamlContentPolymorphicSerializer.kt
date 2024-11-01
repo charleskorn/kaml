521df2ea68a8fb11e1e345f576f8beada41a0082
JojoIV <jonas.broeckmann@gmx.de>
2024-11-01 17:55:55 +0100

    Fix ktlint

--- src/commonMain/kotlin/com/charleskorn/kaml/YamlContentPolymorphicSerializer.kt
+++ src/commonMain/kotlin/com/charleskorn/kaml/YamlContentPolymorphicSerializer.kt
@@ -1,6 +1,11 @@
 package com.charleskorn.kaml
 
-import kotlinx.serialization.*
+import kotlinx.serialization.DeserializationStrategy
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.InternalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.serializerOrNull
+import kotlinx.serialization.SerializationException
 import kotlinx.serialization.descriptors.PolymorphicKind
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.descriptors.buildSerialDescriptor
