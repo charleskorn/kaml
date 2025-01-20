0c15c15252321a5b1a9b1e826725535164ce6896
Piotr Krzeminski <git@krzeminski.it>
2025-01-20 15:45:20 +0100

    Override coroutines version

--- build.gradle.kts
+++ build.gradle.kts
@@ -93,6 +93,9 @@ kotlin {
                 implementation("io.kotest:kotest-assertions-core:6.0.0.M1")
                 implementation("io.kotest:kotest-framework-api:6.0.0.M1")
                 implementation("io.kotest:kotest-framework-engine:6.0.0.M1")
+                // Overridig coroutines' version to solve a problem with WASM JS tests.
+                // See https://kotlinlang.slack.com/archives/CDFP59223/p1736191408326039?thread_ts=1734964013.996149&cid=CDFP59223
+                runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
             }
         }
 
