feb90a1d7ed01b9a9ddde80032a62d9bc00e32f0
Piotr Krzeminski <git@krzeminski.it>
2025-01-27 10:55:48 +0100

    Switch to kotest 6.0.0.1821-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,7 @@ group = "com.charleskorn.kaml"
 
 repositories {
     mavenCentral()
+    maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots")
 }
 
 kotlin {
@@ -90,18 +91,14 @@ kotlin {
 
         commonTest {
             dependencies {
-                implementation("io.kotest:kotest-assertions-core:6.0.0.M1")
-                implementation("io.kotest:kotest-framework-api:6.0.0.M1")
-                implementation("io.kotest:kotest-framework-engine:6.0.0.M1")
-                // Overridig coroutines' version to solve a problem with WASM JS tests.
-                // See https://kotlinlang.slack.com/archives/CDFP59223/p1736191408326039?thread_ts=1734964013.996149&cid=CDFP59223
-                runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+                implementation("io.kotest:kotest-assertions-core:6.0.0.1821-SNAPSHOT")
+                implementation("io.kotest:kotest-framework-engine:6.0.0.1821-SNAPSHOT")
             }
         }
 
         jvmTest {
             dependencies {
-                implementation("io.kotest:kotest-runner-junit5:6.0.0.M1")
+                implementation("io.kotest:kotest-runner-junit5:6.0.0.1821-SNAPSHOT")
             }
         }
     }
