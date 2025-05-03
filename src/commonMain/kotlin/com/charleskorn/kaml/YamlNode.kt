c7cfb43fbbce6d2d001cf14881509795ccc0b599
XYZboom <xyzboom@qq.com>
2025-05-03 20:38:52 +0800

    fix: make YamlMap::get throws IncorrectTypeException with clear message when the type mismatches.

--- src/commonMain/kotlin/com/charleskorn/kaml/YamlNode.kt
+++ src/commonMain/kotlin/com/charleskorn/kaml/YamlNode.kt
@@ -218,11 +218,22 @@ public data class YamlMap(val entries: Map<YamlScalar, YamlNode>, override val p
     override fun contentToString(): String =
         "{" + entries.map { (key, value) -> "${key.contentToString()}: ${value.contentToString()}" }.joinToString(", ") + "}"
 
-    @Suppress("UNCHECKED_CAST")
-    public operator fun <T : YamlNode> get(key: String): T? =
-        entries.entries
+    /**
+     * Returns the value corresponding to the given key and the given type,
+     * or null if such a key is not present in the map,
+     * or throws [IncorrectTypeException] if the value is not the given type.
+     */
+    public inline operator fun <reified T : YamlNode> get(key: String): T? {
+        val node = entries.entries
             .firstOrNull { it.key.content == key }
-            ?.value as T?
+            ?.value ?: return null // no such key in the map
+        // if the value is not the given type,
+        // throws IncorrectTypeException with a clear message instead of ClassCastException.
+        return node as? T ?: throw IncorrectTypeException(
+            "Expected element to be ${T::class.simpleName} but is ${node::class.simpleName}",
+            node.path
+        )
+    }
 
     public fun getScalar(key: String): YamlScalar? = when (val node = get<YamlNode>(key)) {
         null -> null
